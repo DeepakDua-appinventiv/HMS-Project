a1483a8672b82e36ce19c909d772e4d13b485241
DeepakDua-appinventiv <137900559+DeepakDua-appinventiv@users.noreply.github.com>
2023-09-05 18:40:43 +0530

    Update medHistoryRoutes.ts

--- Medical History Feature/medHistoryRoutes.ts
+++ Medical History Feature/medHistoryRoutes.ts
@@ -5,6 +5,7 @@ import { MedicalHistoryClass } from "../controller/medHistoryController";
 
 medRouter.post('/addhistory', staffAuth, MedicalHistoryClass.addHistory);
 medRouter.get('/gethistory', auth, MedicalHistoryClass.getHistory);
+medRouter.get('/getpatienthistory', staffAuth, MedicalHistoryClass.getHistoryForStaff);
 medRouter.put('/updatehistory/:id', staffAuth , MedicalHistoryClass.updateHistory);
 
 export default medRouter;
