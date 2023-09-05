c278f9793332b79f6a3a8764e9641e6b59b87955
DeepakDua-appinventiv <137900559+DeepakDua-appinventiv@users.noreply.github.com>
2023-09-05 18:39:11 +0530

    Update appointmentServices.ts

--- Appointment Feature/appointmentServices.ts
+++ Appointment Feature/appointmentServices.ts
@@ -1,10 +1,94 @@
 import appointmentModel from '../models/appointmentModel';
 import patientModel from '../models/patientModel';
+import DepartmentModel from '../models/departmentModel';
+import staffModel from '../models/staffModel';
 import { ERROR_MESSAGES,RESPONSE_MESSAGES,SUCCESS_MESSAGES } from '../constants';
+import { sendConfirmationEmail } from "../services/emailService";
 import { ObjectId } from 'mongoose';
 
 export class AppointmentServicesClass
 {
+ //Service to handle book appointment
+ static async bookAppointment(patientId: string, appointmentDate: Date, selectedSlot: string, visitReason: string, departmentId: string) {
+    try {
+        // Check if the patient already booked the same slot on the same date
+        const existingAppointment = await appointmentModel.findOne({
+            patientId: patientId,
+            AppointmentDate: appointmentDate,
+            selectedSlot: selectedSlot
+        });
+
+        if (existingAppointment) {
+            return { success: false, message: "You've already booked this appointment slot on the same date." };
+        }
+
+        // Find all doctors within the department from the staff collection
+        const doctorsInDepartment = await staffModel.find({
+            departmentId: departmentId,
+            role: "Doctor"
+        });
+
+        let availableDoctor = null;
+
+        // Check doctor availability for the given slot and date
+        for (const doctor of doctorsInDepartment) {
+            const existingDoctorAppointment = await appointmentModel.findOne({
+                staffId: doctor._id,
+                AppointmentDate: appointmentDate,
+                selectedSlot: selectedSlot
+            });
+
+            if (!existingDoctorAppointment) {
+                availableDoctor = doctor;
+                break;
+            }
+        }
+
+        if (!availableDoctor) {
+            return { success: false, message: "No available doctor found for the selected slot." };
+        }
+
+        // Slot is available and doctor is available, create a new appointment
+        const newAppointment = new appointmentModel({
+            patientId: patientId,
+            AppointmentDate: appointmentDate,
+            selectedSlot: selectedSlot,
+            AppointmentStatus: "Scheduled",
+            visitReason: visitReason,
+            departmentId: departmentId,
+            doctorName: `${availableDoctor.firstName} ${availableDoctor.lastName}`,
+            staffId: availableDoctor._id 
+        });
+
+        await newAppointment.save();
+
+        const department = await DepartmentModel.findById(departmentId);
+        const departmentName = department ? department.name : '';
+
+        const data = {
+            appointmentDate: newAppointment.AppointmentDate,
+            selectedSlot: newAppointment.selectedSlot,
+            doctorName: newAppointment.doctorName,
+            visitReason: newAppointment.visitReason,
+        };
+
+        await sendConfirmationEmail(
+            "deepak.dua@appinventiv.com",
+            "Appointment Booking Success",
+            appointmentDate,
+            selectedSlot,
+            newAppointment.doctorName,
+            departmentName,
+            visitReason
+        );
+
+        return { success: true };
+    } catch (error) {
+        console.error(error);
+        return { success: false, message: "An error occurred while booking the appointment." };
+    }
+}
+ 
  //Service to handle get appointment details
  static async  getAppointmentDetails(pId: ObjectId): Promise<any>{
     try {
