5b8469602be70879c4a883cc343231c523cc4d5d
DeepakDua-appinventiv <137900559+DeepakDua-appinventiv@users.noreply.github.com>
2023-09-05 18:46:01 +0530

    Update patientServices.ts

--- Patient Feature/patientServices.ts
+++ Patient Feature/patientServices.ts
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 import patientModel from "../models/patientModel";
 import { Session } from "../models/sessionModel";
 import { ObjectId } from "mongoose";
-import { createClient } from "redis";
+import { SetOptions, createClient } from "redis";
 import { promises as fsPromises } from "fs";
 import  { ERROR_MESSAGES, RESPONSE_MESSAGES, SUCCESS_MESSAGES } from "../constants";
 import { sendVerifyMail } from "../services/emailService";
@@ -12,6 +12,9 @@ import nodemailer from 'nodemailer';
 import fs from "fs";
 import dotenv from "dotenv";
 dotenv.config();
+const accountSid = 'AC971c22decfabc9d8362241f712808e43';
+const authToken = '45eea03ea99364ab8cd9988372a0650f';
+const TwilioClient = require('twilio')(accountSid, authToken);
 
 const SECRET_KEY = process.env.SECRET_KEY;
 
@@ -33,6 +36,34 @@ export class PatientServicesClass {
         return { status: 200, response: { user: result, message: SUCCESS_MESSAGES.REGISTRATION_SUCCESS } };
       } else {
         return { status: 500, response: { user: result, message: ERROR_MESSAGES.REGISTRATION_FAILED } };
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  //Service to handle verfiy email
+  static async verifyMail(patientId) {
+    try {
+      const findInfo = await patientModel.findOne({
+        $and: [{ _id: patientId }, { verified: true }],
+      });
+      
+      if (findInfo) {
+        return {
+          message: 'Your email has already been verified.',
+          next: 'Please login to the APP.',
+        };
+      } else {
+        const updateInfo = await patientModel.updateOne(
+          { _id: patientId },
+          { $set: { verified: true } }
+        );
+        console.log(updateInfo);
+        return {
+          message: 'Your email has been verified.',
+          next: 'Please login to the APP.',
+        };
       }
     } catch (error) {
       throw error;
@@ -184,6 +215,9 @@ export class PatientServicesClass {
       if (!patient) {
         return { success: false };
       }
+      const client = createClient();
+      client.on("error", (err) => console.log("redis Client Error", err));
+      await client.connect();
       const template = fs.readFileSync('/home/admin446/Desktop/Hospital Mang Project/src/templates/OTP_Email.html', 'utf-8');
       const generateOTP = async (length) => {
         const charset = '0123456789';
@@ -197,7 +231,8 @@ export class PatientServicesClass {
         return otp;
       };
       const otp = await generateOTP(6);
-    
+      const options: SetOptions = { EX: 120 };
+      await client.set(`otp:${pId}`, otp);
       const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
@@ -216,13 +251,69 @@ export class PatientServicesClass {
       };
       
       await transporter.sendMail(mailOptions);
-      const client = createClient();
-      client.on("error", (err) => console.log("redis Client Error", err));
-      await client.connect();
-      await client.set(`status:${pId}`, `${otp}`);
       return { success: true };
     } catch (error) {
       throw error;
         }
+    }
+
+    static async sendPasswordResetOTPSMS(pId: ObjectId): Promise<any> {
+      try {
+        const patient = await patientModel.findOne({ _id: pId });
+        if (!patient) {
+          return { success: false };
+        }
+        const client = createClient();
+        client.on("error", (err) => console.log("redis Client Error", err));
+        await client.connect();
+        const generateOTP = async (length) => {
+          const charset = '0123456789';
+          let otp = '';
+          for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * charset.length);
+            otp += charset[randomIndex];
+          }
+          return otp;
+        };
+        const otp = await generateOTP(6);
+        const options: SetOptions = { EX: 120 };
+        await client.set(`otp:${pId}`, otp);
+  
+        TwilioClient.messages
+        .create({
+          body: `Hi, ${patient.name}, Please use ${otp} as OTP for password reset on your HMS Account. OTP valid for 2 minutes`,
+          from: '+16592014852',
+          to: '+919650949873'
+      })
+      .then(message => console.log(message.sid))
+      .done();
+        
+        return { success: true };
+      } catch (error) {
+        throw error;
+      }
+    }
+
+    //Service to handle reset password functionality
+    static async resetPassword(pId, enteredOtp, newPassword){
+      try {
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        const client = createClient();
+        client.on("error", (err) => console.log("redis Client Error", err));
+        await client.connect();
+        const storedOtp = await client.get(`otp: ${pId}`);
+        if(!storedOtp) {
+          return { success: false };
+        }
+        if(enteredOtp == storedOtp){
+          const patient = await patientModel.findOne({ _id:pId });
+          patient.password = hashedPassword;
+          await patient.save();
+          await client.del(`otp: ${pId}`);
+          return { success: true };
+        }
+      } catch (error) {
+          return { invalidOtp: true };
+      }
     }
 }
