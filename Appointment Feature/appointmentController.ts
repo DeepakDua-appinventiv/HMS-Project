3b4aeb29664899aafc73020a7ba8d9bd7b7b7b3c
DeepakDua-appinventiv <137900559+DeepakDua-appinventiv@users.noreply.github.com>
2023-09-05 18:37:41 +0530

    Update appointmentController.ts

--- Appointment Feature/appointmentController.ts
+++ Appointment Feature/appointmentController.ts
@@ -13,87 +13,31 @@ export class AppointmentClass {
   //book appointment
   static async bookAppointment(req: Request, res: Response): Promise<void> {
     try {
-        const { patientId, appointmentDate, selectedSlot, visitReason, departmentId } = req.body;
+      const {
+        patientId,
+        appointmentDate,
+        selectedSlot,
+        visitReason,
+        departmentId,
+      } = req.body;
+      const result = await AppointmentServicesClass.bookAppointment(
+        patientId,
+        appointmentDate,
+        selectedSlot,
+        visitReason,
+        departmentId
+      );
 
-        // Check if the patient already booked the same slot on the same date
-        const existingAppointment = await appointmentModel.findOne({
-            patientId: patientId,
-            AppointmentDate: appointmentDate,
-            selectedSlot: selectedSlot
-        });
-
-        if (existingAppointment) {
-            res.status(400).json({ message: "You've already booked this appointment slot on the same date." });
-            return;
-        }
-
-        // Find all doctors within the department from the staff collection
-        const doctorsInDepartment = await staffModel.find({
-            departmentId: departmentId,
-            role: "Doctor"
-        });
-
-        let availableDoctor = null;
-
-        // Check doctor availability for the given slot and date
-        for (const doctor of doctorsInDepartment) {
-            const existingDoctorAppointment = await appointmentModel.findOne({
-                staffId: doctor._id,
-                AppointmentDate: appointmentDate,
-                selectedSlot: selectedSlot
-            });
-
-            if (!existingDoctorAppointment) {
-                availableDoctor = doctor;
-                break;
-            }
-        }
-
-        if (!availableDoctor) {
-            res.status(400).json({ message: "No available doctor found for the selected slot." });
-            return;
-        }
-
-        // Slot is available and doctor is available, create a new appointment
-        const newAppointment = new appointmentModel({
-            patientId: patientId,
-            AppointmentDate: appointmentDate,
-            selectedSlot: selectedSlot,
-            AppointmentStatus: "Scheduled",
-            visitReason: visitReason,
-            departmentId: departmentId,
-            doctorName: `${availableDoctor.firstName} ${availableDoctor.lastName}`,
-            staffId: availableDoctor._id 
-        });
-
-        await newAppointment.save();
-
-        const department = await departmentModel.findById(departmentId);
-        const departmentName = department ? department.name : '';
-
-        const data = {
-            appointmentDate: newAppointment.AppointmentDate,
-            selectedSlot: newAppointment.selectedSlot,
-            doctorName: newAppointment.doctorName,
-            visitReason: newAppointment.visitReason,
-        };
-        
-        await sendConfirmationEmail(
-            "deepak.dua@appinventiv.com",
-            "Appointment Booking Success",
-            appointmentDate,
-            selectedSlot,
-            newAppointment.doctorName,
-            departmentName,
-            visitReason
-        );
-
+      if (result.success) {
         res.status(201).json({ message: SUCCESS_MESSAGES.APPOINTMENT_BOOKED });
+      } else {
+        res.status(400).json({ message: result.message });
+      }
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: RESPONSE_MESSAGES.SERVER_ERROR });
+      console.error(error);
+      res.status(500).json({ message: RESPONSE_MESSAGES.SERVER_ERROR });
     }
-}
+  }
 
   //get appointment details API
   static async getAppDetails(req: Request, res: Response): Promise<void> {
